de9e4419ce7c6c8a0c8c6a50bcc63b4da05a372a
16022603 <16022603@myrp.edu.sg>
2018-07-17 16:39:56 +0800

    first commit

--- app/src/main/java/demoshowsms/android/myapplicationdev/com/demofilereadwriting/MainActivity.java
+++ app/src/main/java/demoshowsms/android/myapplicationdev/com/demofilereadwriting/MainActivity.java
@@ -1,13 +1,48 @@
 package demoshowsms.android.myapplicationdev.com.demofilereadwriting;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    String folderLocation;
+    Button btnWrite, btnRead;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        folderLocation = Environment.getExternalStorageDirectory().getAbsolutePath() + "/MyFolder";
+
+        File folder = new File(folderLocation);
+        if(folder.exists() == false){
+            boolean result = folder.mkdir();
+            if(result == true){
+                Log.d("File Read/Write", "Folder created");
+            }
+        }
+
+        btnWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
